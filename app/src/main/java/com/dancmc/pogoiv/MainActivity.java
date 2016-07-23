6956f8ea8bac33aeac475e1d899a8b5c3199e019
dcmc87 <dcmc87@gmail.com>
2016-07-24 04:42:09 +0800

    v0.2.2
    ignored v0.21
    cleaned up the PNG retrieval

--- app/src/main/java/com/dancmc/pogoiv/MainActivity.java
+++ app/src/main/java/com/dancmc/pogoiv/MainActivity.java
@@ -30,12 +30,13 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
 
     private AutoCompleteTextView mPokemonNameInput;
     private TextView mOutputView;
     private Pokemon mPokemon;
     private StringBuilder mStringBuilder;
-    private static final String TAG = "MainActivity";
+
 
     private ArrayList<Pokemon> mPokeballs;
     private final static String POKEBALL_ARRAY = "pokeball_array";
@@ -69,117 +70,118 @@ public class MainActivity extends AppCompatActivity {
             mPokeballs = new ArrayList<Pokemon>(Arrays.asList(new Pokemon[]{null, null, null, null, null, null, null, null}));
         }
         */
+
+        //Retrieves data from database, populates Array & builds pokeballs
         mDataSource = new PokeballsDataSource(this);
         mPokeballs = mDataSource.getAllPokeballs();
         for (int i = 1; i < mPokeballs.size() - 1; i++) {
 
-            if (mPokeballs.get(i) != null) {
-                Log.d(TAG, "onCreate: " + mPokeballs.get(i).toString());
-                buildPokeball(i);
-            }
+            if (mPokeballs.get(i) != null){
+                Log.d(TAG, "onCreate: "+mPokeballs.get(i).toString());
+                buildPokeball(i);}
         }
 
-        //calculate button setup
-        mCalculateButton = (Button) findViewById(R.id.calculate_button);
-        mCalculateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mStringBuilder = new StringBuilder();
+            //calculate button setup
+            mCalculateButton = (Button) findViewById(R.id.calculate_button);
+            mCalculateButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mStringBuilder = new StringBuilder();
 
-                //hides keyboard
-                InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                    //hides keyboard
+                    InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 
-                try {
-                    createPokemonFromInput();
-                } catch (Exception e) {
-                    mOutputView.setText(e.getMessage());
-                    return;
+                    try {
+                        createPokemonFromInput();
+                    } catch (Exception e) {
+                        mOutputView.setText(e.getMessage());
+                        return;
+                    }
+
+
+                    if (mPokemon != null) {
+                        mStringBuilder.append(mPokemon.getStringOutput());
+                    }
+                    mOutputView.setText(mStringBuilder.toString());
+
                 }
+            });
 
+            //Add button setup
+            mAddButton = (ImageButton) findViewById(R.id.fab);
+            mAddButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Add Pokemon")
+                            .setMessage("Do you want to add the current Pokemon to your list?")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(final DialogInterface dialog, int which) {
+                                    //check whether pokemon has been added to any of the 6 pokeballs already
+                                    for (int i = 1; i <= mPokeballs.size() - 1; i++) {
+                                        if (mPokeballs.get(i) != null && mPokeballs.get(i).customEquals(mPokemon)) {
+                                            Toast.makeText(MainActivity.this, "You have already added this Pokemon!", Toast.LENGTH_LONG)
+                                                    .show();
+                                            return;
+                                        }
+                                    }
 
-                if (mPokemon != null) {
-                    mStringBuilder.append(mPokemon.getStringOutput());
-                }
-                mOutputView.setText(mStringBuilder.toString());
-
-            }
-        });
-
-        //Add button setup
-        mAddButton = (ImageButton) findViewById(R.id.fab);
-        mAddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Add Pokemon")
-                        .setMessage("Do you want to add the current Pokemon to your list?")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(final DialogInterface dialog, int which) {
-                                //check whether pokemon has been added to any of the 6 pokeballs already
-                                for (int i = 1; i <= mPokeballs.size() - 1; i++) {
-                                    if (mPokeballs.get(i) != null && mPokeballs.get(i).customEquals(mPokemon)) {
-                                        Toast.makeText(MainActivity.this, "You have already added this Pokemon!", Toast.LENGTH_LONG)
+                                    //reject adding pokemon with no combinations
+                                    if (mPokemon.getNumberOfResults()==0){
+                                        Toast.makeText(MainActivity.this, "Sorry, you can't add Pokemon with no combinations.", Toast.LENGTH_LONG)
                                                 .show();
                                         return;
                                     }
-                                }
 
-                                //reject adding pokemon with no combinations
-                                if (mPokemon.getNumberOfResults() == 0) {
-                                    Toast.makeText(MainActivity.this, "Sorry, you can't add Pokemon with no combinations.", Toast.LENGTH_LONG)
-                                            .show();
-                                    return;
-                                }
+                                    for (int i = 1; i <= mPokeballs.size() - 1; i++) {
+                                        if (i == 7) {
+                                            Toast.makeText(MainActivity.this, "You have no more space", Toast.LENGTH_LONG)
+                                                    .show();
+                                            return;
+                                        } else if (mPokemon == null) {
+                                            Toast.makeText(MainActivity.this, "You have not calculated a Pokemon yet", Toast.LENGTH_LONG)
+                                                    .show();
+                                            return;
 
-                                for (int i = 1; i <= mPokeballs.size() - 1; i++) {
-                                    if (i == 7) {
-                                        Toast.makeText(MainActivity.this, "You have no more space", Toast.LENGTH_LONG)
-                                                .show();
-                                        return;
-                                    } else if (mPokemon == null) {
-                                        Toast.makeText(MainActivity.this, "You have not calculated a Pokemon yet", Toast.LENGTH_LONG)
-                                                .show();
-                                        return;
-
-                                    } else if (mPokeballs.get(i) == null) {
-                                        mPokeballs.set(i, mPokemon);
-                                        buildPokeball(i);
-                                        return;
+                                        } else if (mPokeballs.get(i) == null) {
+                                            mPokeballs.set(i, mPokemon);
+                                            buildPokeball(i);
+                                            return;
+                                        }
                                     }
                                 }
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
+                            })
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
 
-                            }
-                        });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
-        });
+                                }
+                            });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            });
 
-        //compare button setup
-        mCompareButton = (Button) findViewById(R.id.compare_button);
-        mCompareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            //compare button setup
+            mCompareButton = (Button) findViewById(R.id.compare_button);
+            mCompareButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-                String s = mDataSource.compareAllPokeballs(mPokeballs);
-                mOutputView.setText(s);
+                    String s = mDataSource.compareAllPokeballs(mPokeballs);
+                    mOutputView.setText(s);
 
-                //hides keyboard
-                InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-            }
-        });
+                    //hides keyboard
+                    InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow((null == getCurrentFocus()) ? null : getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+            });
 
-    }
+        }
 
-    //reads the input from the EditText views and calls Pokemon constructor
+        //reads the input from the EditText views and calls Pokemon constructor
 
     private void createPokemonFromInput() {
         String pokemonName = "" + mPokemonNameInput.getText().toString();
@@ -187,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
         int hp = parseIntInput(R.id.enter_hp);
         int stardust = parseIntInput(R.id.enter_stardust);
         int level = parseIntInput(R.id.enter_known_level);
-        boolean freshMeat = !((CheckBox) findViewById(R.id.checkbox_powerup)).isChecked();
+        boolean freshMeat = ((CheckBox) findViewById(R.id.checkbox_powerup)).isChecked();
 
         //throws exception if invalid/blank pokemon name, or invalid stardust
         //if HP/CP/Stardust blank, is ok, passes in a -1
@@ -201,17 +203,17 @@ public class MainActivity extends AppCompatActivity {
 
     //returns the integer in an number EditText, or if blank, returns 0
     private int parseIntInput(int editTextID) {
-        int number = 0;
+        int number=0;
         String input = ((EditText) findViewById(editTextID)).getText().toString();
         if (input.equals("")) {
             switch (editTextID) {
                 case (R.id.enter_cp):
                     throw new IllegalArgumentException("You must enter a CP value.");
                 case (R.id.enter_stardust):
-                    mStringBuilder.append("Note : You did not enter a stardust value. All levels calculated.\n");
+                    mStringBuilder.append("Note : You did not enter a stardust value. All levels calculated.\n\n");
                     return -1;
                 case (R.id.enter_hp):
-                    mStringBuilder.append("Note : You did not enter a HP value. All values calculated\n");
+                    mStringBuilder.append("Note : You did not enter a HP value. All values calculated\n\n");
                     return -1;
             }
         } else {
@@ -233,7 +235,7 @@ public class MainActivity extends AppCompatActivity {
         int id = getResources().getIdentifier("pokeball_" + i, "id", getPackageName());
         final ImageButton pokeball = (ImageButton) findViewById(id);
         //set Imagebutton picture
-        int id2 = getResources().getIdentifier((("xxxx" + mPokeballs.get(i).getPokemonNumber()).substring(("" + mPokeballs.get(i).getPokemonNumber()).length())), "drawable", getPackageName());
+        int id2 = getResources().getIdentifier(mPokeballs.get(i).getPngFileName(), "drawable", getPackageName());
         pokeball.setBackgroundResource(R.drawable.circle_background);
         pokeball.setImageResource(id2);
         pokeball.setScaleType(ImageView.ScaleType.CENTER_CROP);
